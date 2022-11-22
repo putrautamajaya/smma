732ac72ffc638d65aa3fdcc3699b7133663068a3
Putra Utama Jaya <putra.utamajaya@ematicsolutions.com>
2022-11-22 16:59:23 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 's-quantum';
+  users: {
+    first_name: string,
+    email: string,
+    avatar: string
+  }[]
+  page: number
+  total_pages: number
+
+  get shouldDisableNext() {
+    return this.page === this.total_pages
+  }
+
+  get shouldDisablePrev() {
+    return this.page === 1
+  }
+
+  constructor(private http: HttpClient) {
+    this.users = []
+    this.page = 1
+    this.total_pages = 1
+  }
+
+  ngOnInit(): void {
+    this.getUsers(this.page)
+  }
+
+  getUsers(page: number) {
+    this.http.get(`https://reqres.in/api/users?page=${page}`).subscribe((data: any) => {
+      this.users = data.data
+      this.total_pages = data.total_pages
+    })
+  }
+
+  nextPage() {
+    this.page = this.page + 1
+    this.getUsers(this.page)
+  }
+
+  prevPage() {
+    this.page = this.page - 1
+    this.getUsers(this.page)
+  }
 }
